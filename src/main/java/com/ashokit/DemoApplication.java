34d1d51a5519d163f93fdc4d18207238835c5b70
Arvindra <tech.arvindra720@gmail.com>
2021-06-04 13:16:43 +0530

    modified DemoApplication variable

--- src/main/java/com/ashokit/DemoApplication.java
+++ src/main/java/com/ashokit/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
        
         int j=20;
+        int k=90;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
